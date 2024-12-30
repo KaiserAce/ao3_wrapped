5972c485a7b16c51798df5c6e1c5eb4cea868c44
KaiserAce <lester.ignacio1209@gmail.com>
2024-12-30 15:52:33 +0800

    Webscraoer finished; data processing draft done

--- main.py
+++ main.py
@@ -19,11 +19,13 @@ else:
     exit()
 
 load_dotenv()
+username = os.getenv("USER_NAME")
+password = os.getenv("PASSWORD")
 print(os.getenv("USER_NAME"), os.getenv("PASSWORD"))
 
 payload = {
-    "user[login]": os.getenv("USER_NAME"),
-    "user[password]": os.getenv("PASSWORD"),
+    "user[login]": username,
+    "user[password]": password,
     "authenticity_token": token,
     "commit": "Log in",
 }
@@ -35,6 +37,8 @@ headers = {
 
 login_response = session.post(login_url, data=payload, headers=headers)
 
+fic_data = []
+
 if login_response.status_code == 200:
     print("Login attempt completed.")
 
@@ -43,7 +47,7 @@ if login_response.status_code == 200:
 
         for i in range(1, 3):
             time.sleep(5)
-            new_page_response = session.get(f'https://archiveofourown.org/users/Kaiser_Ace/readings?page={i}')
+            new_page_response = session.get(f'https://archiveofourown.org/users/{username}/readings?page={i}')
             if new_page_response.status_code == 200:
                 print("Successfully accessed the new page.")
 
@@ -52,16 +56,145 @@ if login_response.status_code == 200:
                 raw_data = page.find("ol", class_="reading work index group")
                 raw_indiv_items = raw_data.find_all("li", role="article")
                 for item in raw_indiv_items:
+                    if item.find("div", class_="header module") == None:
+                        continue
+
                     header = item.find("div", class_="header module")
                     heading = header.find("h4")
                     title = heading.find_all("a")[0].text
-                    print(title)
+                    raw_author = heading.find_all("a", rel="author")
+                    author = []
+                    if raw_author:
+                        for entry in raw_author:
+                            author.append(entry.text)
+                    else:
+                        author.append('Anonymous')
+
+                    fandom_header = header.find("h5")
+                    raw_fandoms = fandom_header.find_all("a", class_="tag")
+                    fandoms = []
+                    for fandom in raw_fandoms:
+                        fandoms.append(fandom.text)
+
+                    required_tags = header.find("ul", class_="required-tags")
+                    raw_ratings = required_tags.find_all("li")[0]
+                    raw_pairings = required_tags.find_all("li")[2]
+                    ratings = raw_ratings.find("span", class_="text").text
+                    pairings = raw_pairings.find("span", class_="text").text
+
+                    raw_warnings = item.find("ul", class_="tags commas").find_all("li", class_="warnings")
+                    raw_relationships = item.find("ul", class_="tags commas").find_all("li", class_="relationships")
+                    raw_characters = item.find("ul", class_="tags commas").find_all("li", class_="characters")
+                    raw_freeforms = item.find("ul", class_="tags commas").find_all("li", class_="freeforms")
+
+                    warnings = []
+                    relationships = []
+                    characters = []
+                    freeforms = []
+
+                    for warning in raw_warnings:
+                        warnings.append(warning.find("a", class_="tag").text)
+                    for relationship in raw_relationships:
+                        relationships.append(relationship.find("a", class_="tag").text)
+                    for character in raw_characters:
+                        characters.append(character.find("a", class_="tag").text)
+                    for tag in raw_freeforms:
+                        freeforms.append(tag.find("a", class_="tag").text)
+
+                    raw_stats = item.find("dl", class_="stats")
+
+                    lang = raw_stats.find("dd", class_="language").text
+                    words = int(raw_stats.find("dd", class_="words").text.replace(",", ""))
+
+                    raw_footer = item.find("h4", class_="viewed heading").text.split("\n")
+                    listed_footer = list(filter(lambda x: x.strip(), raw_footer))
+                    raw_date = listed_footer[0].split(" ")
+                    raw_read_count = listed_footer[-1].split(" ")
+                    date = int(raw_date[-1])
+                    read_count = 0
+                    if raw_read_count[-2].isnumeric():
+                        read_count = int(raw_read_count[-2])
+                    else:
+                        read_count = 1
+
+                    fic_data.append({
+                        'title': title,
+                        'author(s)': author,
+                        'fandom(s)': fandoms,
+                        'ratings': ratings,
+                        'pairings': pairings,
+                        'warnings': warnings,
+                        'relationships': relationships,
+                        'characters': characters,
+                        'freeforms': freeforms,
+                        'language': lang,
+                        'word_count': words,
+                        'last_read': date,
+                        'read_count': read_count,
+                    })
             else:
                 print("Page traversal failed")
                 exit()
-
-
     else:
         print("Login failed. Check your credentials or the token.")
 else:
     print(f"Login request failed with status code {login_response.status_code}")
+
+print(fic_data)
+
+most_read = sorted(fic_data.copy(), key=lambda x: x['read_count'], reverse=True)
+
+print("Most read fics")
+for fic in most_read:
+    print(fic['title'], fic['read_count'])
+
+word_count = 0
+for fic in fic_data:
+    word_count = word_count + fic['word_count']
+print(f"Total Word Count: {word_count}")
+
+print(f"Total fic count: {len(fic_data)}")
+
+tag_count = {}
+for fic in fic_data:
+    for tag in fic['freeforms']:
+        if tag in tag_count.keys():
+            tag_count[tag] += 1
+        else:
+            tag_count[tag] = 1
+
+sorted_tags = dict(sorted(tag_count.items(), key=lambda x: x[1], reverse=True))
+print(sorted_tags)
+
+relationship_count = {}
+for fic in fic_data:
+    for rel in fic['relationships']:
+        if rel in relationship_count.keys():
+            relationship_count[rel] += 1
+        else:
+            relationship_count[rel] = 1
+
+sorted_rels = dict(sorted(relationship_count.items(), key=lambda x: x[1], reverse=True))
+print(sorted_rels)
+
+ratings_count = {}
+for fic in fic_data:
+    if fic['ratings'] in ratings_count.keys():
+        ratings_count[fic['ratings']] += 1
+    else:
+        ratings_count[fic['ratings']] = 1
+
+sorted_rates = dict(sorted(ratings_count.items(), key=lambda x: x[1], reverse=True))
+print(sorted_rates)
+
+
+character_count = {}
+for fic in fic_data:
+    for char in fic['characters']:
+        if char in tag_count.keys():
+            character_count[char] += 1
+        else:
+            character_count[char] = 1
+
+sorted_chars = dict(sorted(character_count.items(), key=lambda x: x[1], reverse=True))
+print(sorted_chars)
