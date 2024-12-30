ccd486120b6dd0cb281fae38e3b30570604f7524
KaiserAce <lester.ignacio1209@gmail.com>
2024-12-30 23:54:58 +0800

    Final data processed

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ else:
 load_dotenv()
 username = os.getenv("USER_NAME")
 password = os.getenv("PASSWORD")
-print(os.getenv("USER_NAME"), os.getenv("PASSWORD"))
 
 payload = {
     "user[login]": username,
@@ -140,20 +139,18 @@ if login_response.status_code == 200:
 else:
     print(f"Login request failed with status code {login_response.status_code}")
 
-print(fic_data)
-
-most_read = sorted(fic_data.copy(), key=lambda x: x['read_count'], reverse=True)
-
-print("Most read fics")
-for fic in most_read:
-    print(fic['title'], fic['read_count'])
+key_val = [2024]
+filtered_data = list(filter(lambda x: x['last_read'] in key_val, fic_data))
+fic_data = filtered_data
 
 word_count = 0
 for fic in fic_data:
     word_count = word_count + fic['word_count']
 print(f"Total Word Count: {word_count}")
+print()
 
 print(f"Total fic count: {len(fic_data)}")
+print()
 
 tag_count = {}
 for fic in fic_data:
@@ -164,8 +161,12 @@ for fic in fic_data:
             tag_count[tag] = 1
 
 sorted_tags = dict(sorted(tag_count.items(), key=lambda x: x[1], reverse=True))
-print(sorted_tags)
+print("Top tags")
+for tag in sorted_tags.keys():
+    print(f"{tag}: {sorted_tags[tag]}")
+print()
 
+print("Top Relationships")
 relationship_count = {}
 for fic in fic_data:
     for rel in fic['relationships']:
@@ -175,8 +176,11 @@ for fic in fic_data:
             relationship_count[rel] = 1
 
 sorted_rels = dict(sorted(relationship_count.items(), key=lambda x: x[1], reverse=True))
-print(sorted_rels)
+for rels in sorted_rels.keys():
+    print(f"{rels}: {sorted_rels[rels]}")
+print()
 
+print("Top Ratings")
 ratings_count = {}
 for fic in fic_data:
     if fic['ratings'] in ratings_count.keys():
@@ -185,9 +189,11 @@ for fic in fic_data:
         ratings_count[fic['ratings']] = 1
 
 sorted_rates = dict(sorted(ratings_count.items(), key=lambda x: x[1], reverse=True))
-print(sorted_rates)
+for rates in sorted_rates.keys():
+    print(f"{rates}: {sorted_rates[rates]}")
+print()
 
-
+print("Favorite Characters")
 character_count = {}
 for fic in fic_data:
     for char in fic['characters']:
@@ -197,4 +203,42 @@ for fic in fic_data:
             character_count[char] = 1
 
 sorted_chars = dict(sorted(character_count.items(), key=lambda x: x[1], reverse=True))
-print(sorted_chars)
+for char in sorted_chars.keys():
+    print(f"{char}: {sorted_chars[char]}")
+print()
+
+print("Top Fandoms")
+fandom_count = {}
+for fic in fic_data:
+    for fan in fic['fandom(s)']:
+        if fan in fandom_count.keys():
+            fandom_count[fan] += 1
+        else:
+            fandom_count[fan] = 1
+
+sorted_fandom = dict(sorted(fandom_count.items(), key=lambda x: x[1], reverse=True))
+for fan in sorted_fandom.keys():
+    print(f"{fan}: {sorted_fandom[fan]}")
+print()
+
+print("Longest fics read")
+longest_fic = sorted(fic_data.copy(), key=lambda x: x['word_count'], reverse=True)
+for fic in longest_fic:
+    print(f"{fic['title']}: {fic['word_count']}")
+print()
+
+print("Top warnings")
+warnings_count = {}
+for fic in fic_data:
+    for warn in fic['warnings']:
+        if warn in warnings_count.keys():
+            warnings_count[warn] += 1
+        else:
+            warnings_count[warn] = 1
+
+sorted_warnings = dict(sorted(warnings_count.items(), key=lambda x: x[1], reverse=True))
+for warn in sorted_warnings.keys():
+    print(f"{warn}: {sorted_warnings[warn]}")
+print()
+
+
